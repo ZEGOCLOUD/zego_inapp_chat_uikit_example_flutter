523142ae53eefb2440285f2d4d434d552fdf8559
nemoyuan <nemoyuan@zego.im>
2023-06-27 18:47:12 +0800

    Update home_page.dart

--- basic_demo/lib/home_page.dart
+++ basic_demo/lib/home_page.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
 
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
 import 'package:zego_zimkit/zego_zimkit.dart';
 
 import 'package:zego_zimkit_demo/home_page_popup.dart';
@@ -31,13 +32,20 @@ class ZIMKitDemoHomePage extends StatelessWidget {
                               iconSize: const Size(40, 40),
                               buttonSize: const Size(50, 50),
                               isVideoCall: isVideoCall,
-                              invitees: [ZegoUIKitUser(id: conversation.id, name: conversation.name)],
-                              onPressed: (String code, String message, List<String> errorInvitees) {
-                                onCallInvitationSent(context, code, message, errorInvitees);
+                              invitees: [
+                                ZegoUIKitUser(
+                                    id: conversation.id,
+                                    name: conversation.name)
+                              ],
+                              onPressed: (String code, String message,
+                                  List<String> errorInvitees) {
+                                onCallInvitationSent(
+                                    context, code, message, errorInvitees);
                               },
                             ),
                         ]
                       : [],
+                  onMessageItemLongPress: _onMessageItemLongPress,
                 );
               },
             ));
@@ -47,8 +55,9 @@ class ZIMKitDemoHomePage extends StatelessWidget {
     );
   }
 
-  void onCallInvitationSent(BuildContext context, String code, String message, List<String> errorInvitees) {
-    late String log;
+  void onCallInvitationSent(BuildContext context, String code, String message,
+      List<String> errorInvitees) {
+    var log = '';
     if (errorInvitees.isNotEmpty) {
       log = "User doesn't exist or is offline: ${errorInvitees[0]}";
       if (code.isNotEmpty) {
@@ -57,8 +66,53 @@ class ZIMKitDemoHomePage extends StatelessWidget {
     } else if (code.isNotEmpty) {
       log = 'code: $code, message:$message';
     }
+    if (log.isEmpty) {
+      return;
+    }
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(content: Text(log)),
+    );
+  }
+
+  Future<void> _onMessageItemLongPress(
+    BuildContext context,
+    LongPressStartDetails details,
+    ZIMKitMessage message,
+    Function defaultAction,
+  ) async {
+    showCupertinoDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: const Text('Confirme'),
+          content: const Text('Delete or recall this message?'),
+          actions: [
+            CupertinoDialogAction(
+              onPressed: Navigator.of(context).pop,
+              child: const Text('Cancel'),
+            ),
+            CupertinoDialogAction(
+              onPressed: () {
+                ZIMKit().deleteMessage([message]);
+                Navigator.pop(context);
+              },
+              child: const Text('Delete'),
+            ),
+            CupertinoDialogAction(
+              onPressed: () {
+                ZIMKit().recallMessage(message).catchError((error) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text(error.toString())),
+                  );
+                });
+                Navigator.pop(context);
+              },
+              child: const Text('Recall'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
